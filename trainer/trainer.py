4d11f7cc48bca68054576d64a9442331b9e1c47b
blackpudding96 <mitcbj@daum.net>
2021-12-08 10:54:51 +0900

    MODIFY : Save and log based on step

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,9 +1,9 @@
+import wandb
+import warnings
 import numpy as np
 import torch
 import torch.nn as nn
-import warnings
 from tqdm import tqdm
-from torchvision.utils import make_grid
 from base import BaseTrainer
 from utils import inf_loop, MetricTracker
 
@@ -24,72 +24,96 @@ class Trainer(BaseTrainer):
         self.valid_data_loader = valid_data_loader
         self.do_validation = self.valid_data_loader is not None
         self.lr_scheduler = lr_scheduler
-        self.log_step = int(np.sqrt(data_loader.batch_size))
 
         self.train_metrics = MetricTracker('train/loss', *['train/' + m.__name__ for m in self.metric_ftns])
         self.valid_metrics = MetricTracker('val/loss', *['val/' + m.__name__ for m in self.metric_ftns])
 
-    def _train_epoch(self, epoch):
+    def train(self):
         """
         Training logic for an epoch
 
         :param epoch: Integer, current training epoch.
         :return: A log that contains average loss and metric in this epoch.
         """
+        step = 0
         self.model.train()
-        self.train_metrics.reset()
         
-        for step, data in enumerate(tqdm(self.data_loader, desc=f'TRAINING - [{epoch}] EPOCH')):
-            input_ids, token_type_ids, attention_mask, targets = data
-            
-            input_ids = input_ids.to(self.device)
-            attention_mask = attention_mask.to(self.device)
-            token_type_ids = token_type_ids.to(self.device)
-            targets = targets.to(self.device)
+        for epoch, _ in enumerate(range(self.epochs), start = 1):
+            for _, data in enumerate(tqdm(self.data_loader, desc=f'TRAINING - [{epoch}] EPOCH')):
+                step += 1
+                
+                input_ids, token_type_ids, attention_mask, targets = data
+                
+                input_ids = input_ids.to(self.device)
+                attention_mask = attention_mask.to(self.device)
+                token_type_ids = token_type_ids.to(self.device)
+                targets = targets.to(self.device)
 
-            self.optimizer.zero_grad()
+                self.optimizer.zero_grad()
+                
+                inputs = {
+                    "input_ids": input_ids,
+                    "attention_mask": attention_mask,
+                    "token_type_ids": token_type_ids
+                }
+                outputs = self.model(inputs)
+                
+                if isinstance(outputs, torch.Tensor):
+                    logits = outputs
+                else:
+                    logits = outputs[0]
             
-            inputs = {
-                "input_ids": input_ids,
-                "attention_mask": attention_mask,
-                "token_type_ids": token_type_ids
-            }
-            outputs = self.model(inputs)
+                _, preds = torch.max(logits, dim=1)
+                
+                loss = self.criterion(logits, targets)
+                loss.backward()
+                
+                # https://curiousily.com/posts/sentiment-analysis-with-bert-and-hugging-face-using-pytorch-and-python/
+                # avoding exploding gradients
+                nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
+                
+                self.optimizer.step()
+                if self.lr_scheduler is not None:
+                    self.lr_scheduler.step()
+                
+                preds = preds.detach().cpu().numpy()
+                targets = targets.detach().cpu().numpy()
+                
+                self.train_metrics.update('train/loss', loss.item())
+                for met in self.metric_ftns:
+                    self.train_metrics.update('train/' + met.__name__, met(preds, targets))
+
+                # activate validation and saving when current step meets 'save steps'
+                if step % self.save_steps == 0:
+                    log = self.train_metrics.result()
+                    
+                    if self.do_validation:
+                        val_log = self._validation(step)
+                        log.update(**{k : v for k, v in val_log.items()})
+                    
+                    log['epoch'] = epoch
+                    log['steps'] = step
+                    
+                    # visualization log
+                    wandb.log(log, step=step)
             
-            logits = outputs[0]
-            _, preds = torch.max(logits, dim=1)
-            
-            loss = self.criterion(logits, targets)
-            loss.backward()
-            
-            # https://curiousily.com/posts/sentiment-analysis-with-bert-and-hugging-face-using-pytorch-and-python/
-            # avoding exploding gradients
-            nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
-            
-            self.optimizer.step()
-            if self.lr_scheduler is not None:
-                self.lr_scheduler.step()
-            
-            preds = preds.detach().cpu().numpy()
-            targets = targets.detach().cpu().numpy()
+                    for key, value in log.items():
+                        self.logger.info('    {:15s}: {}'.format(str(key), value))
+                    
+                    is_best = self._evaluate_performance(log)
+                    
+                    # Early Stopping
+                    if self.not_improved_count > self.early_stop:
+                        self.logger.info("Validation performance didn\'t improve for {} epochs. ""Training stops.".format(self.early_stop))
+                        return False
+                    
+                    self._save_checkpoint(log, is_best)
+                    
+                    # get back to work again!
+                    self.model.train()
+                    self.train_metrics.reset()
 
-            self.train_metrics.update('train/loss', loss.item())
-            for met in self.metric_ftns:
-                self.train_metrics.update('train/' + met.__name__, met(preds, targets))
-
-            if step % self.log_step == 0:
-                self.logger.debug('Train Loss: {:.6f}'.format(
-                    loss.item()))
-
-        log = self.train_metrics.result()
-
-        if self.do_validation:
-            val_log = self._valid_epoch(epoch)
-            log.update(**{k : v for k, v in val_log.items()})
-
-        return log
-
-    def _valid_epoch(self, epoch):
+    def _validation(self, step):
         """
         Validate after training an epoch
 
@@ -100,7 +124,8 @@ class Trainer(BaseTrainer):
         self.valid_metrics.reset()
         
         with torch.no_grad():
-            for step, data in enumerate(tqdm(self.valid_data_loader, desc=f'VALIDATION - [{epoch}] EPOCH')):
+            print(f"VALIDATION - [{step}] STEPS ...")
+            for _, data in enumerate(self.valid_data_loader):
                 input_ids, token_type_ids, attention_mask, targets = data
             
                 input_ids = input_ids.to(self.device)
@@ -115,7 +140,11 @@ class Trainer(BaseTrainer):
                 }
                 outputs = self.model(inputs)
                 
-                logits = outputs[0]
+                if isinstance(outputs, torch.Tensor):
+                    logits = outputs
+                else:
+                    logits = outputs[0]
+                    
                 _, preds = torch.max(logits, dim=1)
                 loss = self.criterion(logits, targets)
                 
