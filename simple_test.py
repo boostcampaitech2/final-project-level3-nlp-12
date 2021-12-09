1450e03c991eafdc91773f4bdf9bc96f40498431
jadon <didwodnr123@gmail.com>
2021-12-09 08:29:16 +0000

    FIX: no gpu error and check inference time

--- simple_test.py
+++ simple_test.py
@@ -3,9 +3,12 @@ import torch
 from utils.util import read_json
 import argparse
 import pprint
+import time
 
 
 def main(config):
+    device = 0 if torch.cuda.is_available() else -1
+
     model_path = config.model_path
     config_path = config.config_path
 
@@ -19,11 +22,19 @@ def main(config):
         config=model_config,
         model=model.model,
         tokenizer=tokenizer,
+        device=device
     )
 
     for i in range(config.num):
-        pprint.pprint(pipe(input(f"문장을 입력하세요 {i+1} / {config.num}: ")))
+        text = input(f"문장을 입력하세요 {i+1} / {config.num}: ")
 
+        start_time = time.time()
+        result = pipe(text)
+        end_time = time.time()
+        
+        print(f'inference time: {end_time - start_time}')
+        pprint.pprint(result)
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -51,7 +62,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-n",
         "--num",
-        default=1,
+        default=3,
         type=int,
         help="How many times will you check the sentence? (default: 1)",
     )
