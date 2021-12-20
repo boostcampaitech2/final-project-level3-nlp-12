586a39062303e84ac0b593a8778d51f03fffbaef
LeeYeonGeol <f2f42012@gmail.com>
2021-12-20 21:04:32 +0900

    FIX: path conflict

--- prototype/fullstack/app/predict.py
+++ prototype/fullstack/app/predict.py
@@ -1,4 +1,5 @@
-from transformers import AutoTokenizer, pipeline
+from transformers import pipeline, AutoTokenizer, AutoConfig, AutoModelForSequenceClassification
+
 import torch
 # import 01-streamlit as st
 from utils import read_json
@@ -8,37 +9,38 @@ from utils import read_json
 def get_pipeline():
     device: int = 0 if torch.cuda.is_available() else -1
 
-    model_path: str = '/Users/yangjaeug/Desktop/GitHub/Product-Serving/assets/comments_task/model.pt'
-    config_path: str = '/Users/yangjaeug/Desktop/GitHub/Product-Serving/practice/03-streamlit-fastapi/app/config.json'
+    #model_path: str = '/Users/yangjaeug/Desktop/GitHub/Product-Serving/assets/comments_task/model.pt'
+    #config_path: str = '/Users/yangjaeug/Desktop/GitHub/Product-Serving/practice/03-streamlit-fastapi/app/config.json'
 
-    model = torch.load(model_path, map_location=torch.device('cpu'))
-    model_config = read_json(config_path)
+    model = AutoModelForSequenceClassification.from_pretrained("beomi/beep-KcELECTRA-base-hate")
 
-    tokenizer = AutoTokenizer.from_pretrained('beomi/KcELECTRA-base')
+    model_config = AutoConfig.from_pretrained('beomi/beep-KcELECTRA-base-hate')
+
+    tokenizer = AutoTokenizer.from_pretrained('beomi/beep-KcELECTRA-base-hate')
 
     pipe = pipeline(
         task="text-classification",
         config=model_config,
-        model=model.model,
+        model=model,
         tokenizer=tokenizer,
         device=device
     )
-    
+
     return pipe
 
 
 def inference(data, pipe):
     results = []
-    
+
     for d in data:
         try:
             output = pipe(d['comment'])
         except:
             continue
-    
+
         if output[0]['label'] == 'none':
             continue
-    
+
         else:
             results.append(
                 {
@@ -51,5 +53,5 @@ def inference(data, pipe):
                     'commented_at': d['commented_at']
                 }
             )
-    
+
     return results
