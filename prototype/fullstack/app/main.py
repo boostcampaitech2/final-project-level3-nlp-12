b39b4d0c83814984a3a67e867d58d80698316973
양재욱_T2130 <didwodnr123@gmail.com>
2021-12-23 16:48:27 +0900

    Update main.py

--- prototype/fullstack/app/main.py
+++ prototype/fullstack/app/main.py
@@ -1,18 +1,25 @@
 from fastapi import FastAPI
-from requests.api import get
-from torch.utils.data import DataLoader
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import StreamingResponse
 import uvicorn
+
 from predict import load_model, inference, load_dataloader
 from database import run_db, insert2db
-from load_data import load_data, retrieve_comments, NhDataloader
-from utils import make_samples
+from load_data import load_data, retrieve_comments
+
+from wordcloud import WordCloud, STOPWORDS
+from io import BytesIO
+from pydantic import BaseModel
 
 
 app = FastAPI()
 dataset = load_data()
 model, classifier = load_model()
-evidence = run_db()
+# evidence = run_db()
 
+class Data(BaseModel):
+    comment: str
+    keyword: str
 
 @app.get('/')
 def hello_world():
@@ -22,15 +29,15 @@ def hello_world():
 @app.get('/get_sample/{keyword}')
 def get_sample(keyword):
     data = retrieve_comments(keyword, dataset)
-    inf_dataloader = load_dataloader(data[:128])
+    inf_dataloader = load_dataloader(data)
     results = inference(model, classifier, inf_dataloader)
     
-    res2json = []
-    for i, res in enumerate(results[:5]):
+    docs = []
+    for i, res in enumerate(results):
         if res == 0:
             continue
         else:
-            res2json.append({
+            docs.append({
                 "keyword": keyword,
                 'user_id': data[i]['user_id'],
                 'comment': data[i]['comment'],
@@ -40,11 +47,41 @@ def get_sample(keyword):
                 'commented_at': data[i]['commented_at']
             })
   
-    if res2json:
-        insert2db(keyword, res2json, evidence)
+    # if docs:
+    #     insert2db(keyword, docs, evidence)
   
-    return res2json
+    return docs[:5]
+
+# @app.get('/get_count/{keyword}')
+# def get_count(keyword):
+#     cnt = len(evidence.find({'keyword': keyword}))
+#     return {'count': cnt}
+
+@app.post("/wordcloud/", description="wordcloud를 생성합니다.")
+def wordcloud(json: Data):
+    json_compatible_item_data = jsonable_encoder(json)
+
+    stopwords = set(STOPWORDS)
+    stopwords.add(json_compatible_item_data['keyword'])
+    stopwords.add(json_compatible_item_data['keyword']+'는')
+    stopwords.add(json_compatible_item_data['keyword']+'가')
+    stopwords.add(json_compatible_item_data['keyword']+'도')
+    word_cloud = (
+        WordCloud(
+            width=500, height=500, stopwords=stopwords
+        )
+        .generate(json_compatible_item_data["comment"])
+        .to_image()
+    )
+
+    img = BytesIO()
+    word_cloud.save(img, "PNG")
+
+    img.seek(0)
+    return StreamingResponse(img, media_type="image/PNG")
 
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)  
+    # result = get_sample('유재석')
+    # print(result)
