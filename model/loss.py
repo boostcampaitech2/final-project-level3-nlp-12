a6b2123b1860df30da23602729d3355c4ee77aa3
blackpudding96 <mitcbj@daum.net>
2021-12-24 23:34:22 +0900

    ADD : Knowledge Distillation

--- model/loss.py
+++ model/loss.py
@@ -9,3 +9,13 @@ def nll_loss(output, target):
 def softmax(output, target):
     loss = nn.CrossEntropyLoss()
     return loss(output, target)
+
+def knowledge_distillation_loss(logits, target, teacher_logits):
+        alpha = 0.3
+        T = 1
+        
+        student_loss = F.cross_entropy(input=logits, target=target)
+        distillation_loss = nn.KLDivLoss(reduction='batchmean')(F.log_softmax(logits/T, dim=1), F.softmax(teacher_logits/T, dim=1)) * (T * T)
+        total_loss =  (1. - alpha)*student_loss + alpha*distillation_loss
+
+        return total_loss
--- trainer/__init__.py
+++ trainer/__init__.py
@@ -1,1 +1,2 @@
 from .trainer import *
+from .kd_trainer import *
